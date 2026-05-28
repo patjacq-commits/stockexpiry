95d9ac27c399744a6c71691d16f496a8d6373717
patjacq-commits <patjacq@skynet.be>
2026-05-28 12:13:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE='stockexpiry-v9';
-const ASSETS=['./index.html','./css/style.css','./js/app.js','./js/data.js','./manifest.json','./icons/esso-192.png','./icons/esso-512.png'];
+const CACHE='stockexpiry-v10';
+const ASSETS=['./index.html','./css/style.css','./js/app.js','./js/data.js','./manifest.json','./icons/esso2-192.png','./icons/esso2-512.png'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch',e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));
 });
